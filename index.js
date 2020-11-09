b18e1d59abcae5fd8d20fabb3c3df07448dcf3ed
Tanja Olsson <tanja.olsson@skola.botkyrka.sem>
2020-11-09 14:21:07 +0100

    uppgift 1/2

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Read instructions. And remove this line.
+function RandomInt(maxValue) {
+    const x = Math.random() * maxValue
+    return Math.floor(x);
+}
+
+console.log(RandomInt(101))
+
+function getUserGuess() {
+    const stringValue = document.getElementById('user-input').value
+    return parseInt(stringValue, 10)
+}
+getUserGuess()
+
+globalThis.getUserGuess = getUserGuess;
